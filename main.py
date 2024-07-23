7e121214447f9ba98ed7a1dabbbddd243f7aa788
FACE6911 <boss.haydar@bk.ru>
2024-07-23 17:12:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 print(9 ** 0.5 * 5, 9.99 > 9.98 and 1000 != 1000.1) # 1st & 2nd program
 print((1234%1000//10) + (5678%1000//10)) # 3rd program
-print(int(13.42) == (int(42.13*100%4200)) and int(42.13) == (int(13.42*100%1300))) # 4th program
+print(int(13.42) == (int(42.13*100%100)) and int(42.13) == (int(13.42*100%100))) # 4th program
